c571df9dd7c31afdb1dd5687d6b2b16f87a639de
Jed Saxon <jed@peterjacksons.com>
2025-10-30 15:53:45 +1100

    log queries

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module pjwish
 
 go 1.25.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/ping", PingHandler)
+	r := mux.NewRouter()
 
+	r.HandleFunc("/ping", PingHandler).Methods(http.MethodGet)
+	r.HandleFunc("/ping", PostPingHandler).Methods(http.MethodPost)
 	log.Printf("serving requests on 0.0.0.0:5000")
-	http.ListenAndServe("0.0.0.0:5000", nil)
+
+	http.ListenAndServe("0.0.0.0:5000", r)
 }
 
 func PingHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,4 +23,27 @@ func PingHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json; utf-8")
 	w.Write([]byte("pong"))
+}
+
+func PostPingHandler(w http.ResponseWriter, r *http.Request) {
+	decoded, err := json.MarshalIndent(r.Header, "", "  ")
+
+	if err != nil {
+		log.Printf("unable to decode headers: %v", err)
+		internalServerError(w)
+	}
+
+	log.Printf("headers: %s", decoded)
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json; utf-8")
+	w.Write([]byte("pong"))
+}	
+
+func internalServerError(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusInternalServerError)
+	if _, err := w.Write([]byte(`{"message": "internal server error"}`)); err != nil{
+		log.Printf("unable to respond to request: %v", err)
+		return
+	}
 }
